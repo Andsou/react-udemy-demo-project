2538910d0226c2a89baa024f6e5eb407e4f388fa
andsou <andysoutcharith@gmail.com>
2023-01-23 14:56:50 -0600

    Added functionality to App.js when delete button is clicked a modal window shows up

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Master react" />
+      <Todo text="Explore the full course" />
     </div>
   );
 }
