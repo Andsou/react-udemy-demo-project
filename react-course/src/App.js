f6dab1269a94e33f17f9f615201df80e80c61511
andsou <andysoutcharith@gmail.com>
2023-01-23 14:33:13 -0600

    Added a Todo function to simplify the code

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div>
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
