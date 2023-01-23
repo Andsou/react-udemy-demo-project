6f0c3842341a693ba706ed1a5ae676b99b4073ff
andsou <andysoutcharith@gmail.com>
2023-01-23 15:29:43 -0600

    Deleted and modified files so that there is a blank page for a new project

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,14 +1,5 @@
-import Todo from "./todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master react" />
-      <Todo text="Explore the full course" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
