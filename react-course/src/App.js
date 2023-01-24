3e8c64191384c51e8203cd4c6cc84ab0afba291b
andsou <andysoutcharith@gmail.com>
2023-01-24 10:56:13 -0600

    Updated App.js and added content to the main page

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -2,22 +2,22 @@ import { Route, Switch } from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
-import FavouritesPage from "./pages/Favourites";
+import FavoritesPage from "./pages/Favourites";
 import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
-        <MainNavigation />
         <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
         <Route path="/new-meetup">
           <NewMeetupPage />
         </Route>
-        <Route path="/favourites">
-          <FavouritesPage />
+        <Route path="/favorites">
+          <FavoritesPage />
         </Route>
       </Switch>
     </div>
