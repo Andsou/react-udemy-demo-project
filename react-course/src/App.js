c4f3c522523319c9e43adf045faa5bbb2dccd1ed
andsou <andysoutcharith@gmail.com>
2023-01-24 09:43:52 -0600

    Added links to the main page

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavouritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
       <Switch>
+        <MainNavigation />
         <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
